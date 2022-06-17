88bdc3112e7ea8447be6da781bbbc8656132a5b1
hyunsung kim <hyunsungkim@hyunsungui-MacBookPro.local>
2022-06-18 05:48:07 +0900

    Comments APIs update

--- src/api/comments/removeComment.ts
+++ src/api/comments/removeComment.ts
@@ -2,18 +2,18 @@ import type { Context } from 'koa';
 import bcrypt from 'bcryptjs';
 import Joi from 'joi';
 import Comment from '../../entities/Comment';
-import { loadUser, validateBody } from '../../libs/utils';
+import { validateBody } from '../../libs/utils';
 import { dataSource } from '../../server';
 
 async function removeCommentAPI(ctx: Context) {
   const { id }: { id: string } = ctx.params;
 
   type RequestType = {
-    password?: string;
+    password: string;
   };
 
   const schema = Joi.object().keys({
-    password: Joi.string(),
+    password: Joi.string().required(),
   });
 
   if (!validateBody(ctx, schema)) return;
@@ -21,7 +21,6 @@ async function removeCommentAPI(ctx: Context) {
   const { password }: RequestType = ctx.request.body;
 
   try {
-    const user_id = await loadUser(ctx);
     const commentsRepo = await dataSource.getRepository(Comment);
     const comment = await commentsRepo.findOneBy({ id });
 
@@ -31,20 +30,12 @@ async function removeCommentAPI(ctx: Context) {
       return;
     }
 
-    if (password) {
-      const valid = await bcrypt.compare(password, comment.comment_password);
+    const valid = await bcrypt.compare(password, comment.comment_password);
 
-      if (!valid) {
-        ctx.status = 403;
-        ctx.body = '비밀번호가 틀렸습니다.';
-        return;
-      }
-    } else {
-      if (!user_id || user_id === '') {
-        ctx.status = 403;
-        ctx.body = '관리자가 아닙니다.';
-        return;
-      }
+    if (!valid) {
+      ctx.status = 403;
+      ctx.body = '비밀번호가 틀렸습니다.';
+      return;
     }
 
     await commentsRepo.delete({ id });
